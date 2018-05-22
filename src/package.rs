1052eae030b81fe4bcf8d38d4f885afe51645462
Imani Dap <imani.dap@gmail.com>
2018-05-22 11:19:32 +0200

    relation between weight, fragile & delivery_delay

--- src/package.rs
+++ src/package.rs
@@ -6,9 +6,9 @@ use customer;
 #[derive(RustcDecodable, RustcEncodable, Debug)]
 pub struct Package {
     id: i64,
-    delivery_delay: i64,
+    delivery_delay: i64, //minutes
     //arrival_time: i64, //TODO: Make this happen!!
-    weight: i64, //max 100
+    weight: i64,
     fragile: bool, 
     dimensions: Dimensions,
     customer: customer::Customer,
@@ -25,11 +25,14 @@ pub fn generate() -> Package {
     //find out dimention ranges & structure of package ID's from fedex mock data
     let mut random = rand::thread_rng();
 
+    let package_weight : i64 = random.gen_range(0, 68); //kilos
+    let is_fragile : bool = random.gen::<bool>();
+
     return Package {
         id: random.gen_range(0, 250),
-        delivery_delay: random.gen_range(300, 1200), 
-        weight: random.gen_range(0, 100),
-        fragile: random.gen::<bool>(),
+        delivery_delay: generate_delivery_delay(package_weight, is_fragile),
+        weight: package_weight,
+        fragile: is_fragile,
         dimensions: generate_dimentions(),
         customer: customer::generate(), //TODO: find a way to link multiple packages to the same customer
     };
@@ -54,4 +57,22 @@ fn generate_dimentions() -> Dimensions {
         width: random.gen_range(0, 50),
         depth: random.gen_range(0, 100),
     };
+}
+
+fn generate_delivery_delay(weight : i64, fragile : bool) -> i64 {
+    let multiplier : f64 = 0.1233;
+    let casted_weight : f64 = weight as f64;
+    let weight_delay : f64 = multiplier * casted_weight.powf(2.0); 
+    let weight_delay_sec : i64 = weight_delay.round() as i64;
+
+    let mut fragile_delay;
+
+    //NOTE: this could obviously be a match 
+    if fragile {
+        fragile_delay = 1;
+    } else {
+        fragile_delay = 0;
+    }
+
+    return weight_delay_sec + (30 * fragile_delay);
 }
